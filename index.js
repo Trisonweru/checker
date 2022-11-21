3dfdbfa1f56ae972de683e7d22be9260ed85765a
Joe Doh <joedoh@Joes-MacBook-Pro.local>
2022-11-21 18:00:19 +0300

    fr

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 var request = require("request");
 const axios = require("axios");
-var mysql = require('mysql2');
+var mysql = require('mysql');
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -15,16 +15,13 @@ const Logs = require("./models/logs");
 
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 
 const server = http.createServer(app);  
 let interval;
 
 const io = socketIo(server, {
   cors: {
-    origin: "https://safaribust.co.ke",
+    origin: "http://localhost:3001",
   },
 });
 
@@ -47,6 +44,10 @@ const getApiAndEmit = (socket) => {
                   user: "bustadmin_dbadm",
                   password: ";,bp~AcEX,*a",
                   database:"bustadmin_paydb",
+                  multipleStatements : true,
+                  connectionLimit: 10,
+                  acquireTimeout: 30000,
+                  minConnections: 1,
                 });
                   con.connect(function(err) {
                     if (err) throw err;
@@ -55,32 +56,26 @@ const getApiAndEmit = (socket) => {
                         Object.keys(result).forEach(async function(key) {
                         var row = result[key];
                         const transaction= await Transaction.findOne({trans_id:row.trans_id})
-
                         if(transaction){
                           const response = {deposited: false};                            
                           io.sockets.emit("FromAPI2", response);
-                          return 
+                          return
                         }
-                          const account = await Account.findOne({ phone:row.bill_ref_number});
-                          const user = await User.findOne({ phone:row.bill_ref_number});
-                          account.balance=user.label === "1"? parseFloat(+account?.balance) + parseFloat(+row.trans_amount)*2:user.label === "2"||user.label === "3" ? parseFloat(+account?.balance) + parseFloat(+row.trans_amount):parseFloat(+account?.balance)
-                          user.label =user.label="1"&&"2"
-                          user.firstDeposit =user.label ==="1"&& parseFloat(+row.trans_amount).toFixed(2)
-                          const av_log = await Logs.findOne({ transactionId:row.trans_id});
+                          ids.push(row.trans_id)
                           const trans= new Transaction({
                                   type:"Deposit",
                                   trans_id:row.trans_id,
                                   bill_ref_number:row.bill_ref_number,
                                   trans_time:row.trans_time,
                                   amount:row.trans_amount,
-                                  phone: row.bill_ref_number,
-                                  username:user.username,
-                                  balance:account.balance
+                                  phone: row.bill_ref_number
                             })
-                          await account.save()
                           await trans.save()
-                          await user.save()
-
+                          const account = await Account.findOne({ phone:row.bill_ref_number});
+                          account.balance=parseFloat(account?.balance) + parseFloat(row.trans_amount)
+                          await account.save()
+                          const user = await User.findOne({ phone:row.bill_ref_number});
+                          const av_log = await Logs.findOne({ transactionId:row.trans_id});
                           if(!av_log){
                               const log = new Logs({
                                   ip: "deposit",
@@ -92,12 +87,13 @@ const getApiAndEmit = (socket) => {
                           }
                           const response = {
                                 deposited: true,
+                                trans_id:transaction.trans_id
                               };
                            io.sockets.emit("FromAPI2", response);
                          return 
                      });
                 })
-              con.end(()=>console.log("connection closed"))              
+              con.end(()=>console.log("connection closed"))
             });
           }catch(err){
           console.log(err)
